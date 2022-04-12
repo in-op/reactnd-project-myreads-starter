75f51829808eb2335f10e7d0994f4e217e094bcd
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2022-04-11 23:27:10 -0400

    Fix syntax error with background image url

--- src/Books.js
+++ src/Books.js
@@ -6,7 +6,11 @@ class Book extends Component {
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `'url("${imageURL}")'` }}></div>
+                    <div className="book-cover" style={{
+                        width: 128,
+                        height: 193,
+                        backgroundImage: `url("${imageURL}")`,
+                    }}></div>
                     <div className="book-shelf-changer">
                     <select>
                         <option value="move" disabled>Move to...</option>
