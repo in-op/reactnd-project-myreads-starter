f51f0fff2263383fbc81ee166dce47313b4ca926
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2022-05-16 20:15:37 -0400

    Use Link to nav back to main page from search page

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import BookGrid from './BookGrid';
 
 class SearchPage extends Component {
@@ -6,9 +7,11 @@ class SearchPage extends Component {
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>
-                        Close
-                    </button>
+                    <Link to="/">
+                        <button className="close-search">
+                            Close
+                        </button>
+                    </Link>
                     <div className="search-books-input-wrapper">
                         { }
                         <input type="text" placeholder="Search by title or author"/>
